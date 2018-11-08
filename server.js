08f0039e812917eb6a9340688ae9b91cca5e7213
Tay J Solis <tayjsolis@gmail.com>
2018-11-08 12:26:56 -0800

    All get endpoints completed

--- server.js
+++ server.js
@@ -23,8 +23,15 @@ app.get('/', (req, res) => {
     });
 });
 
-  //Read: Get all species in database
-app.get('/api/orders', (req, res) => {
+//Allows urls to take params of any case
+const caseFixName =(name)=>{
+    name = name.toLowerCase()
+    name = name.charAt(0).toUpperCase() + name.substr(1);
+    return name;
+}
+
+//Read: Get all orders in a database
+app.get('/api/order/all', (req, res) => {
     db.Order.find({})
       .populate({ path: 'families', model: db.Family,
       populate: {
@@ -42,8 +49,28 @@ app.get('/api/orders', (req, res) => {
       });
   });
 
+//Read: Get order by name
+app.get('/api/order/:name', (req, res) => {
+    db.Order.findOne({name: caseFixName(req.params.name)})
+      .populate({ path: 'families', model: db.Family,
+      populate: {
+          path: 'genus',
+          model: db.Genus,
+          populate: {
+            path: 'species',
+            model: db.Species,
+          }
+      },
+  })
+      .exec((err, order) => {
+        if (err) throw err;
+        res.json(order)
+      });
+  });
 
-app.get('/api/families/', (req, res)=>{
+
+//Read: Get all families in a database
+app.get('/api/family/all', (req, res)=>{
     db.Family.find({})
     .populate({ path: 'genera', model: db.Genus,
         populate: {
@@ -57,7 +84,24 @@ app.get('/api/families/', (req, res)=>{
     })
 })
 
-app.get('/api/genera/', (req,res) =>{
+//Read: Get family by name
+app.get('/api/family/:name', (req, res) => {
+    db.Family.findOne({name: caseFixName(req.params.name)})
+      .populate({ path: 'genera', model: db.Genus,
+        populate: {
+            path: 'species',
+            model: db.Species
+        },
+    })
+    .exec(function (err, doc) {
+        if (err) throw err;
+        res.send(doc);
+    })
+});
+
+
+//Read: Get all genera in a database
+app.get('/api/genus/all', (req,res) =>{
     db.Genus.find({})
     .populate({path: 'species', model: db.Species})
     .exec(function (err, doc) {
@@ -66,6 +110,17 @@ app.get('/api/genera/', (req,res) =>{
     })
 })
 
+//Read: Get genus by name
+app.get('/api/genus/:name', (req, res) => {
+    db.Genus.findOne({name: caseFixName(req.params.name)})
+    .populate({path: 'species', model: db.Species})
+    .exec(function (err, doc) {
+        if (err) throw err;
+        res.send(doc);
+    })
+});
+
+//Read: Get a species by common name
 app.get('/api/species/:commonName', (req,res) =>{
     db.Species.findOne({commonName: req.params.commonName})
     .populate({path: 'genus', model: db.Genus})
